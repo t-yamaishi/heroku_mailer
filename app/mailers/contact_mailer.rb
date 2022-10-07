fc27fb3f181167342e2c2dcc4bdc477d0bd28f2f
yamaishi <yamakios8@icloud.com>
2022-10-07 14:47:02 +0900

    actionmailer　heroku3

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to: @contact.email, subject: "お問い合わせ確認メール"
+    mail to: yamakios8@icloud.com, subject: "お問い合わせ確認メール"
   end
 end
