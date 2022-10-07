3bba4b9706ef629896c7b1da6b1e5e7c2ae25ce0
yamaishi <yamakios8@icloud.com>
2022-10-07 14:09:14 +0900

    actionmailer　heroku

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'yamakios8@icloud.com'
+  layout 'mailer'
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  resources :contacts
+end
